096bc959e0706d3eb72ac04537261dfeffa15198
Phillip Ou <pou@mit.edu>
2016-12-06 01:46:54 -0500

    Refactored button.

--- react/Elements/BottomRightButton.jsx
+++ react/Elements/BottomRightButton.jsx
@@ -8,17 +8,18 @@ class BottomRightButton extends Component {
 
   render() {
     const title = this.props.title ? this.props.title : "OK";
-    const style = { position: 'fixed',
-                    bottom: 25,
-                    right: 25,
-                    borderRadius: "50%",
-                    width:"70px",
-                    height:"70px",
-                    backgroundColor: "#66B110",
-                    color:"white",
-                    border: "none"}
+    const style = {   position: 'fixed',
+      bottom: '25px',
+      right: '25px',
+      borderRadius: '50%',
+      width:'70px',
+      height:'70px',
+      background: ' #66B110 url(../../public/assets/GreyPencil.svg) no-repeat',
+      color:'white',
+      border: 'none'
+                  }
     return (
-      <button className="BottomRightButton" onClick={this.props.onClick.bind(null)}>{title}</button>
+      <button style = {style} className="BottomRightButton" onClick={this.props.onClick.bind(null)}>{title}</button>
     );
   }
 }
