27a33ad3dc949c624e0626f5de21b027f1369ed2
Phillip Ou <pou@mit.edu>
2016-12-07 20:59:01 -0500

    Imported drag and drop icon.

--- react/Elements/Cards/StandbyItemCard.jsx
+++ react/Elements/Cards/StandbyItemCard.jsx
@@ -9,10 +9,32 @@ class StandbyItemCard extends Component {
   }
 
   render() {
+    var multiCardIcon = require("../../../public/assets/MultiCard.svg");
+    var iconStyle = {
+      float:"left",
+      width:"45px",
+      height:"45px",
+      display:"inline-block",
+      marginBottom:"0px",
+      marginTop: "5px",
+      marginLeft:"10px",
+      marginRight:"10px"
+    }
+
+    var titleStyle = {
+      float:"none",
+      display:"inline-block",
+      marginBottom:"0px"
+    }
 		return (
       <a href="#" onClick = {this.onClick.bind(this)}>
         <div className = "StandbyItemCard">
+
+          <img style = {iconStyle} src = {multiCardIcon} />
           <p className = "ItemCardTitle">{"Drag & Drop Options"}</p>
+
+
+
           <p className = "ItemCardDescription">{"to update rankings"}</p>
         </div>
       </a>
